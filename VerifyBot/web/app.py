d9b158f3391bc369f64d78c431aa528bd0c83c3d
25ksm <zero060315@cu.ac.kr>
2025-07-09 15:27:59 +0900

    Add files via upload

--- VerifyBot/web/app.py
+++ VerifyBot/web/app.py
@@ -9,7 +9,6 @@ from .shared.spreadsheet import update_spreadsheet
 
 import requests
 import os
-import traceback
 
 # ① instance_relative_config=True 로 플라스크 인스턴스 폴더 사용
 app = Flask(__name__, template_folder="templates", instance_relative_config=True)
@@ -27,16 +26,11 @@ def consent():
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    try:
-        discord_id = request.form.get("discord_id")
-        username = request.form.get("username")
-        joined_at = request.form.get("joined_at")
-
-        save_user_info(discord_id, username, joined_at)
-        return render_template("success.html")
-
-    except Exception as e:
-        return f"에러 발생: {str(e)}", 500
+    discord_id = request.form.get("discord_id")
+    username = request.form.get("username")
+    joined_at = request.form.get("joined_at")
+    save_user_info(discord_id, username, joined_at)
+    return render_template("success.html")
 
 @app.route("/admin")
 def admin():
