c81874c110912d469f140c255822c38227096c01
25ksm <zero060315@cu.ac.kr>
2025-08-17 02:38:54 +0900

    Delete VerifyBot-main directory

--- VerifyBot-main/VerifyBot/bot/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-AUTH_WEB_URL = os.getenv("AUTH_WEB_URL", "http://localhost:5000")
-PORT = int(os.getenv("PORT", 5000))
